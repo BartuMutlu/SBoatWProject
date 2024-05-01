eebb15280cde33bc6cba01d2c36b2dbfb303fced
Alan <Alan.mori.abrams@gmail.com>
2024-04-30 21:09:27 -0700

    Updated Sprtites and tileset
    Update from 04/29

---
+++ Blob.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+ #Look up a video on how to make the sprite move on its own 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
